03c0bbfcc46cda77f41a61128c8347e30c7b1455
Mukesh Rana <mukeshotwalarana@gmail.com>
2026-06-21 13:49:04 +0530

    Create login screen with mobile and Google sign in

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,27 +1,37 @@
-import { ImageBackground ,StyleSheet, Text, View,Image } from 'react-native'
+import { ImageBackground ,StyleSheet, Text, View,Image, TextInput } from 'react-native'
 import React from 'react'
 import Button from '../components/Button';
 
 const LoginScreen = () => {
   return (
     <View style={{flex:1}}>
-        <ImageBackground source={require('../assets/img/splash.png')} style={{flex:1, justifyContent: 'center'}}>
-       <View style={{flex:0.6,justifyContent:'center',marginTop:120,alignItems:'center'}}>
-        <Image source={require('../assets/img/expert.png')} style={styles.image} resizeMode='contain'>
-        </Image>
+        <View style={{flex:1,justifyContent: 'center',backgroundColor:'#300aec'}} >
+       <View style={{flex:0.6,justifyContent:'center',marginTop:20,alignItems:'center'}}>
+        <Image source={require('../assets/img/facility.png')} style={{width:'100%',height:'100%'}} resizeMode='contain' />
 
        </View>
        <View style={{flex:0.45,backgroundColor:'white',borderTopLeftRadius:20,borderTopRightRadius:20}}>
          <View style={{justifyContent:'center',alignItems:'center'}}>
-            <Text style={{fontSize:24,fontWeight:'bold',paddingVertical:10}}>Connect with Top Experts</Text>
-            <Text style={{fontSize:18,paddingVertical:10,alignSelf:'center',paddingHorizontal:10,textAlign:'center',color:'gray'}}>Book one-on-one sessions with verified professionals anytime, anywhere.</Text>
+            <Text style={{fontSize:24,fontWeight:'bold',paddingVertical:10}}>Log in or Sign up</Text>
          </View>
-        
+         <View style={{flexDirection:'row',height:50,borderColor:'gray',borderWidth:2,alignItems:'center',margin:10 ,borderRadius:10 }}>
+          <Text style={{fontSize:18,paddingHorizontal:10}} >{'+91'} </Text>
+          <TextInput style={{padding:10,fontSize:18}} keyboardType='numeric' placeholder='Phone Number' maxLength={10} />
+           
+
+         </View >
+         <View style={{width:'100%',paddingVertical:15, paddingHorizontal:10}}>
+         <Button label={"Login with Mobile"} style={{backgroundColor:'#0b0ff7'}} />
+        </View>
        </View>
        <View style={{bottom:0,position:'absolute',width:'100%',padding:10}}>
-         <Button label={"Sign In"} style={{backgroundColor:'#0b0ff7'}} />
+        <Button style={{backgroundColor:'#e5e4ef',flexDirection:'row',gap:4,borderColor:'black',borderWidth:2}}>
+          <Image source={require('../assets/img/google_icon.png')} />
+          <Text style={{color:'black',fontSize:18}}>Continue with Google</Text>
+
+        </Button>
        </View>
-        </ImageBackground>
+        </View>
     </View>
   )
 }
@@ -29,8 +39,4 @@ const LoginScreen = () => {
 export default LoginScreen
 
 const styles = StyleSheet.create({
-    image: {
-    width: '100%',
-    height: 900,
-  },
 })
